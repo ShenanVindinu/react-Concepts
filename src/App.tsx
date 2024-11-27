37b0b42594f48d10fa54ef3798fb289259b82bca
ShenanVindinu <shenanvindinu@gmail.com>
2024-11-27 12:08:48 +0530

    for loop with data passing

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,38 @@ import Item from "./component/Item.tsx";
 
 function App() {
 
+    const itemList = [
+        {
+            title: 'RAD',
+            desc: 'Lorem ipsum dolor'
+        },
+        {
+            title: 'RAD',
+            desc: 'Lorem ipsum dolor sit amet'
+        },
+        {
+            title: 'RAD',
+            desc: 'Lorem dolor sit amet'
+        }
+    ]
 
+    //for loop with data passing
+    const itemsArray = [];
 
-  return (
-    <div>
-        <Item title={itemList[0].title} desc={itemList[0].desc} />
-        <Item title={itemList[1].title} desc={itemList[1].desc} />
-        <Item title={itemList[2].title} desc={itemList[2].desc} />
-    </div>
-  )
-}
-
-const itemList = [
-    {
-        title: 'RAD',
-        desc: 'Lorem ipsum dolor'
-    },
-    {
-        title: 'RAD',
-        desc: 'Lorem ipsum dolor sit amet'
-    },
-    {
-        title: 'RAD',
-        desc: 'Lorem dolor sit amet'
+    for (const itemsArrayElement of itemList) {
+        itemsArray.push(
+            <Item title={itemsArrayElement.title} desc={itemsArrayElement.desc} />
+        );
     }
-]
+
+    return (
+        //same for loop function with arrow function. less complicated
+        <div>
+            {itemList.map(item =>(
+                <Item title={item.title} desc={item.desc} />
+            ))}
+        </div>
+    )
+}
 
 export default App
