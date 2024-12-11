a2344c459f2443ab9273b7ea45c969f7033402cc
ShenanVindinu <shenanvindinu@gmail.com>
2024-12-11 10:15:34 +0530

    add name via button

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,37 @@ function App() {
         lastName: "",
     })
 
-    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    // function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    //     setCustomer({
+    //         ...customer,
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
+
+    function addCustomer() {
         setCustomer({
             ...customer,
-            [e.target.name]: e.target.value
+            firstName: firstName,
+            lastName: lastName
         })
     }
 
     return (
         <>
-           <input name={'firstName'} type="text" placeholder="First Name" onChange={handleInputChange} />
-           <input name={'lastName'} type="text" placeholder="Last Name" onChange={handleInputChange} />
+            <input name={'firstName'} type="text" placeholder="First Name"
+                   onChange={(e) => setFirstName(e.target.value)}/>
+            <input name={'lastName'} type="text" placeholder="Last Name"
+                   onChange={(e) => setLastName(e.target.value)}/>
+
+            <br/>
+            <br/>
+
+            <button onClick={addCustomer}>Submit</button>
+
+            <br/>
             <br/>
             {customer.firstName + " " + customer.lastName}
         </>
